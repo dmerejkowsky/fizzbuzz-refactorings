a169a76ebf0f3a85cb8ca7121e6cfb9b7310a3a9
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-08-29 11:03:32 +0200

    Improve comments

--- java/src/main/java/fr/arolla/FizzBuzz.java
+++ java/src/main/java/fr/arolla/FizzBuzz.java
@@ -2,24 +2,10 @@ package fr.arolla;
 
 
 public class FizzBuzz {
-  /**
-   * Implements the fizzbuzz game
-   *
-   * @param number the number
-   * @return the output
-   * if number is divisible by 3:
-   * return "fizz"
-   * if number is divisible by 5:
-   * return "buzz"
-   * if number is divisible by 15:
-   * return "fizzbuzz"
-   * else
-   * return the number a string
-   */
   public static String fizzBuzz(int number) {
     boolean divisibleBy3 = number % 3 == 0;
     boolean divisibleBy5 = number % 5 == 0;
-
+    // Note: keep this test first
     if (divisibleBy3 && divisibleBy5) {
       return "fizzbuzz";
     }
