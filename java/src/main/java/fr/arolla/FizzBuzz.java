9b24e446c29617c4d8eb86cc90fb29286cf8a3a4
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-08-29 11:01:13 +0200

    No more `else` keyword

--- java/src/main/java/fr/arolla/FizzBuzz.java
+++ java/src/main/java/fr/arolla/FizzBuzz.java
@@ -19,13 +19,15 @@ public class FizzBuzz {
   public static String fizzBuzz(int number) {
     if (number % 3 == 0 && number % 5 == 0) {
       return "fizzbuzz";
-    } else if (number % 3 == 0) {
-      return "fizz";
-    } else if (number % 5 == 0) {
-      return "buzz";
-    } else {
-      return Integer.toString(number);
     }
+    if (number % 3 == 0) {
+      return "fizz";
+    }
+    if (number % 5 == 0) {
+      return "buzz";
+    }
+
+    return Integer.toString(number);
   }
 
   public static void main(String[] args) {
